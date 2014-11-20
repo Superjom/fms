ee4beb109bdd9ca14bb06cde460ef0d84631710e
superjom <superjom@gmail.com>
2014-11-20 22:15:30 +0800

    update sgd function

--- src/core/sgd/PairSigmoidSGD.h
+++ src/core/sgd/PairSigmoidSGD.h
@@ -3,15 +3,15 @@
 #include "BaseSGD.h"
 namespace fms {
 
-class PairSGD : public BaseSGD<ListInstance> {
+class PairSigmoidSGD : public BaseSGD<ListInstance> {
 public:
     enum comp_t {    // pair两个元素间的大小关系
         GREATER = 2,
         EQUAL = 1,
         LOWER = 0
     };
-    explicit PairSGD() { }
-    explicit PairSGD(AdaGradFMParam &fm) : _fm(&fm)
+    explicit PairSigmoidSGD() { }
+    explicit PairSigmoidSGD(AdaGradFMParam &fm) : _fm(&fm)
     {
         CHECK(this->_fm != nullptr);
     }
@@ -38,14 +38,14 @@ public:
                     label = EQUAL;
                 }
                 //cout << "label:\t" << label << endl;
-                double q, o12;
-                double cost = forward(label, pre, next, pre_x_v_sum, next_x_v_sum, q, o12);
+                double q, o12, score1, score2;
+                double cost = forward(label, pre, next, pre_x_v_sum, next_x_v_sum, q, o12, score1, score2);
                 //cout << "cost\t" << cost << endl;
                 /*if(std::isnan(cost)) {
                     // 忽略 Nan 记录
                     continue;
                 }*/
-                backward(label, pre, next, pre_x_v_sum, next_x_v_sum, q, o12);
+                backward(label, pre, next, pre_x_v_sum, next_x_v_sum, q, o12, score1, score2);
                 total_cost.cumulate(cost);
             }
         }
@@ -72,8 +72,8 @@ public:
                 } else if (pre.target == next.target) {
                     label = EQUAL;
                 }
-                double q, o12;
-                double cost = forward(label, pre, next, pre_x_v_sum, next_x_v_sum, q, o12);
+                double q, o12, score1, score2;
+                double cost = forward(label, pre, next, pre_x_v_sum, next_x_v_sum, q, o12, score1, score2);
                 if(std::isnan(cost)) {
                     // 忽略 Nan 记录
                     return cost;
@@ -85,9 +85,9 @@ public:
         return total_cost.norm();
     }
 protected:
-    double forward(comp_t label, const Instance &pre, const Instance &next, Vec &pre_x_v_sum, Vec &next_x_v_sum, double &q, double &o12) {
-        double o1 = cal_ins_fm_score(pre, pre_x_v_sum);
-        double o2 = cal_ins_fm_score(next, next_x_v_sum);
+    double forward(comp_t label, const Instance &pre, const Instance &next, Vec &pre_x_v_sum, Vec &next_x_v_sum, double &q, double &o12, double &score1, double &score2) {
+        double o1 = cal_ins_fm_score(pre, pre_x_v_sum, score1);
+        double o2 = cal_ins_fm_score(next, next_x_v_sum, score2);
         //cout << "o1:o2\t" << o1 << "\t" << o2 << endl;
         o12 = o1 - o2;
         //cout << "o12\t" << o12 << endl;
@@ -104,7 +104,7 @@ protected:
             (label == LOWER && o12 < 0.0) ) cost = 0.0;
         return cost;
     }
-    void backward(comp_t label, const Instance &pre, const Instance &next, Vec &pre_x_v_sum, Vec &next_x_v_sum, double q, double o12) {
+    void backward(comp_t label, const Instance &pre, const Instance &next, Vec &pre_x_v_sum, Vec &next_x_v_sum, double q, double o12, double score1, double score2) {
         const double &p = (double)label / 2.0;
         double exp_o12 = exp(o12);
         double grad_C_q = - p / q + (1 - p) / (1 - q);
@@ -114,12 +114,13 @@ protected:
         double grad_C_o2 = grad_C_q * grad_q_o12 * grad_o12_o2;
 
         // pre
-        backward_ins(grad_C_o1, pre, pre_x_v_sum);
-        backward_ins(grad_C_o2, next, next_x_v_sum);
+        backward_ins(grad_C_o1, pre,  pre_x_v_sum, score1);
+        backward_ins(grad_C_o2, next, next_x_v_sum, score2);
     }
 
 protected:
-    double cal_ins_fm_score(const Instance &ins, Vec &x_v_sum) {
+    // calculate fm score with lr score
+    double cal_ins_fm_score(const Instance &ins, Vec &x_v_sum, double &score) {
         double x_v2sum{0.0}; // (xv)^2
         double lr_score = 0.0;
         //const double &p = ins.target;
@@ -135,26 +136,23 @@ protected:
             x_v2sum += dot((x * v), (x * v));
         }
         double fm_score = 0.5 * (dot(x_v_sum, x_v_sum) - x_v2sum);
-        //cout << "fm_score\t" << fm_score << endl;
-        //cout << "lr_score\t" << lr_score << endl;
-        //lr_score = 0.0;
-        //double o = lr_score + fm_score;
-        double o = fm_score;
-        return o;
+        score = lr_score + fm_score;
+        return 1.0 / (1.0 + exp(-score));
     }
-    void backward_ins(double grad_C_o, const Instance &ins, Vec &x_v_sum) {
+    void backward_ins(double grad_C_o, const Instance &ins, Vec &x_v_sum, double score) {
+        double grad_o_score = exp(score) / pow((1 + exp(score)), 2);
         SGDGradValue grad(_fm->dim());
         for(auto it = ins.feas.begin(); it != ins.feas.end(); ++it) {
             const index_t &key = it->key;
             const double &x = it->value;
-            double grad_o_lr = x;
+            double grad_score_lr = x;
             FMValue &feature = _fm->feature(key);
             Vec &v = feature.fm_w;
-            Vec grad_o_v(_fm->dim());
-            grad_o_v = x * (x_v_sum - x * v);
-            double grad_C_lr = grad_C_o * grad_o_lr;
+            Vec grad_score_v(_fm->dim());
+            grad_score_v = x * (x_v_sum - x * v);
+            double grad_C_lr = grad_C_o * grad_o_score * grad_score_lr;
             //cout << "grad_C_lr\t" << grad_C_lr << endl;
-            Vec grad_C_v = grad_C_o * grad_o_v;
+            Vec grad_C_v = grad_C_o * grad_o_score * grad_score_v;
             //cout << "grad_C_v\t" << grad_C_v << endl;
             grad.set(grad_C_lr, grad_C_v);
             _fm->batch_commit(key, grad);
